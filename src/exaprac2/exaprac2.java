34e76e5e1af9ee99eb81e0d8f9cc7130b836797f
sandra1187 <50397860+sandra1187@users.noreply.github.com>
2019-05-11 22:08:48 -0600

    Update exaprac2.java

--- src/exaprac2/exaprac2.java
+++ src/exaprac2/exaprac2.java
@@ -30,10 +30,11 @@ public class exaprac2 {
             nombre[i]=JOptionPane.showInputDialog("Nombre del empleado " + (i+1)); 
             horas[i]=Integer.parseInt(JOptionPane.showInputDialog("Horas Trabajadas: "));
             salario[i]=Double.parseDouble(JOptionPane.showInputDialog("Salario X Hora: "));
-            total += horas[i] * salario[i];  
-            detalle += "Empleado: " + nombre[i] + " Horas: " + horas[i] + " Salario: $ " + salario[i] + "\n";  
-        
+            total += horas[i] * salario[i]; 
             subtotal[i] = horas[i] * salario[i];
+            detalle += "Empleado: " + nombre[i] + " Horas: " + horas[i] + " Salario: $ " + subtotal[i] + "\n";  
+        
+            
             
             if(subtotal[i]>mayor){
                 mayor=subtotal[i];
